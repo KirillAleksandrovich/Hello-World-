c62c1f96e495b94bffa7baf60ff8eebbab316248
«Kirill» <kirill.feshev@bk.ru>
2022-09-02 00:05:31 +0300

    add Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "викуля")
+{
+    Console.WriteLine("Ура, это же Викуля!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
